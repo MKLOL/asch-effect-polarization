c32346758ce6a185371791b418912b2d43d78ae5
Fabian Spaeh <fspaeh@bu.edu>
2023-10-13 18:09:31 -0400

    fast iterations

--- mse_graph_calculator.py
+++ mse_graph_calculator.py
@@ -1,6 +1,57 @@
 import numpy as np
 import networkx as nx
 
+
+def approximateMseFast(G, slist, resistances=None, max_iterations=100, eps=1e-5):
+    n = len(G.nodes)
+    x = slist
+
+    for i in range(max_iterations):
+        x_new = np.empty(n)
+        for u in G.nodes:
+            x_u = resistances[u] * slist[u]
+            x_vs = 0
+            for v in G.neighbors(u): x_vs += x[v]
+            x_u += (1 - resistances[u]) * x_vs / len(G[u])
+            x_new[u] = x_u
+
+        norm = np.linalg.norm(x - x_new)
+        norm_inf = np.max(np.abs(x - x_new))
+        if norm_inf < eps: break
+        x = x_new
+
+        print(f"Iteration {i}: {norm}")
+
+    # print(x)
+    x_mse = np.var(x)
+    return x_mse
+
+
+
+def approximateMse(G, slist, resistances=None, max_iterations=100, eps=1e-5):
+    n = len(G.nodes)
+    W = nx.adjacency_matrix(G).toarray()
+    W = W / np.sum(W, axis=0)[:, None]
+
+    A = np.diag([0.5] * n if resistances is None else resistances)
+
+    x = slist
+
+    for i in range(max_iterations):
+        x_new = A @ slist + (np.eye(n) - A) @ W @ x
+        norm = np.linalg.norm(x - x_new)
+        norm_inf = np.max(np.abs(x - x_new))
+        if norm_inf < eps: break
+        x = x_new
+
+        # print(f"Iteration {i}: {norm}")
+
+    # print(x)
+    x_mse = np.var(x) # np.mean((x - np.mean(x))**2)
+    return x_mse
+
+
+
 def calculateMse(G, slist, numCompute = 100, resistances = None):
     n = len(G.nodes)
     W = nx.adjacency_matrix(G).toarray()
@@ -20,13 +71,15 @@ def calculateMse(G, slist, numCompute = 100, resistances = None):
         else:
             s = slist
         xs = X @ s
+        # print(xs)
 
         avgs = np.mean(s)
         avgx = np.mean(xs)
-        s_val.append(sum([(x-avgs) ** 2 for x in s]))
-        x_val.append(sum([(x-avgx) ** 2 for x in xs]))
+        s_val.append(np.mean([(x-avgs) ** 2 for x in s]))
+        x_val.append(np.mean([(x-avgx) ** 2 for x in xs]))
 
     s_mse = np.mean(s_val)
     x_mse = np.mean(x_val)
 
     return (s_mse, x_mse)
+
